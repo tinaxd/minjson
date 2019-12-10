97a6900f664c880915d90ed21318f7c336387790
tinaxd <31384595+tinaxd@users.noreply.github.com>
2019-12-10 21:44:04 +0900

    Command line options

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,81 @@
 extern crate minjson;
+extern crate clap;
 
-use std::env;
+use std::fs::File;
+use std::io::Write;
 use std::io::Read;
+use std::io::BufReader;
 
 use std::io;
 
+use clap::{App, Arg};
+
 fn main() -> () {
-	let args: Vec<String> = env::args().collect();
-	if args.len() != 2 || (args[1] != "minify" && args[1] != "pretty") {
-		eprintln!("Wrong number of arguments");
-		eprintln!("Usage: minjson <operation>");
-		eprintln!("<operation> := minify | pretty");
-		return;
-	}
+	let app =App::new("minjson")
+				.version("0.2")
+				.about("JSON tools")
+				.author("tinaxd")
+				.arg(Arg::with_name("mode")
+					.short("m")
+					.long("mode")
+					.required(true)
+					.takes_value(true)
+					.possible_values(&["minify", "pretty"])
+					.value_name("MODE")
+					)
+				.arg(Arg::with_name("out")
+					.long("out")
+					.takes_value(true)
+					.value_name("FILEPATH")
+					)
+				.arg(Arg::with_name("in")
+					.long("in")
+					.takes_value(true)
+					.value_name("FILEPATH"))
+				.get_matches();
+
+	let mode = app.value_of("mode").unwrap();
 
 	let mut strbuf = String::new();
-    io::stdin().read_to_string(&mut strbuf).expect("Unable to read json");
+	if let Some(inpath) = app.value_of("in") {
+		match File::open(inpath) {
+			Err(e) => {
+				eprintln!("{}", e);
+				return;
+			},
 
+			Ok(infile) => {
+				let mut bf = BufReader::new(infile);
+				bf.read_to_string(&mut strbuf).expect("Unable to read json");
+			}
+		}
+	} else {
+		io::stdin().read_to_string(&mut strbuf).expect("Unable to read json");
+	}
+    
     let res: String;
-    if args[1] == "minify" {
+    if mode == "minify" {
     	res = minjson::minimize_json(&strbuf);
-    } else {
+    } else if mode == "pretty" {
     	res = minjson::pretty_json(&strbuf, &minjson::PrettySetting{indent_width: 2});
+    } else {
+    	unreachable!();	
     }
 
-    print!("{}", res);
+    if let Some(outpath) = app.value_of("out") {
+		let outfile = File::create(outpath);
+		if outfile.is_err() {
+			eprintln!("File writing err {}", outfile.unwrap_err().to_string());
+			return;
+		} else {
+    		let mut outfile = outfile.unwrap();
+    		if let Err(e) = write!(outfile, "{}", res) {
+    			eprintln!("{}", e);
+    			return;
+    		}
+		}
+    } else {
+    	// stdout output
+    	print!("{}", res);
+    }
 }
